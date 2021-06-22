71d4cefd3c68641950bf87f0c91a7ca3fd48032f
Jeeiii <giacomo.corrias7@gmail.com>
2021-06-22 12:53:38 +0200

    chore: add ethersjs library; extension of window global object with the ethereum provider interface

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="react-scripts" />
+
+// Extends the global window object with the ethereum property for Metamask provider.
+interface EthereumProvider {
+  isMetaMask?: boolean
+}
+declare global {
+  interface Window {
+    ethereum?: EthereumProvider
+  }
+}
+
+export default global
