6c6b040514f89d6d244f5b4107b2585102fc2b50
Jeeiii <giacomo.corrias7@gmail.com>
2021-07-12 12:57:47 +0200

    chore: add new message when there are no nfts saleable or licensable

--- src/screens/Market.tsx
+++ src/screens/Market.tsx
@@ -124,121 +124,139 @@ export default function MarketPage() {
         {"MARKET"}
       </Typography>
       {_nfts.length > 0 ? (
-        <Grid
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="center"
-        >
-          {_nfts
-            .sort((a: NFT, b: NFT) => a.id - b.id)
-            .filter(
-              (nft: NFT) => nft.sellingPrice > 0 || nft.dailyLicensePrice > 0
-            )
-            .map((nft: NFT, i: number) => (
-              <Box key={i} className={classes.cardsBox}>
-                <Card className={classes.card}>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      alt={nft.metadata.title}
-                      image={nft.metadata.image}
-                      title={nft.metadata.title}
-                      className={classes.cardImage}
-                      onClick={() => history.push(`/market/${nft.id}`)}
-                    />
-                    <CardContent className={classes.cardContent}>
-                      <Box onClick={() => history.push(`/market/${nft.id}`)}>
-                        <Typography
-                          variant="h5"
-                          component="h2"
-                          className={classes.cardText}
-                        >
-                          {nft.metadata.title}
-                        </Typography>
+        <>
+          {_nfts.filter(
+            (nft: NFT) => nft.sellingPrice > 0 || nft.dailyLicensePrice > 0
+          ).length > 0 ? (
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="center"
+            >
+              {_nfts
+                .sort((a: NFT, b: NFT) => a.id - b.id)
+                .filter(
+                  (nft: NFT) =>
+                    nft.sellingPrice > 0 || nft.dailyLicensePrice > 0
+                )
+                .map((nft: NFT, i: number) => (
+                  <Box key={i} className={classes.cardsBox}>
+                    <Card className={classes.card}>
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          alt={nft.metadata.title}
+                          image={nft.metadata.image}
+                          title={nft.metadata.title}
+                          className={classes.cardImage}
+                          onClick={() => history.push(`/market/${nft.id}`)}
+                        />
+                        <CardContent className={classes.cardContent}>
+                          <Box
+                            onClick={() => history.push(`/market/${nft.id}`)}
+                          >
+                            <Typography
+                              variant="h5"
+                              component="h2"
+                              className={classes.cardText}
+                            >
+                              {nft.metadata.title}
+                            </Typography>
 
-                        <Typography
-                          variant="h6"
-                          component="h3"
-                          className={classes.cardText}
-                          style={{ color: "green" }}
-                        >
-                          {Number(formatUnits(nft.sellingPrice.toString()))}{" "}
-                          {"Ξ"} /{" "}
-                          {Number(
-                            formatUnits(nft.dailyLicensePrice.toString())
-                          )}{" "}
-                          {"Ξ"}
-                        </Typography>
-                      </Box>
-                      <Typography
-                        gutterBottom
-                        variant="body2"
-                        className={classes.cardText}
-                        color="textSecondary"
-                        style={{ padding: 0, fontSize: "0.8rem" }}
-                      >
-                        <i>{"List price / Daily license price"}</i>
-                      </Typography>
-
-                      <Divider style={{ backgroundColor: "black" }} />
-
-                      <Box className={classes.ownershipBox}>
-                        <Box className={classes.ownerArtistBox}>
+                            <Typography
+                              variant="h6"
+                              component="h3"
+                              className={classes.cardText}
+                              style={{ color: "green" }}
+                            >
+                              {Number(formatUnits(nft.sellingPrice.toString()))}{" "}
+                              {"Ξ"} /{" "}
+                              {Number(
+                                formatUnits(nft.dailyLicensePrice.toString())
+                              )}{" "}
+                              {"Ξ"}
+                            </Typography>
+                          </Box>
                           <Typography
                             gutterBottom
-                            variant="body1"
+                            variant="body2"
                             className={classes.cardText}
                             color="textSecondary"
-                            style={{
-                              padding: 0,
-                              margin: 0,
-                              fontSize: "0.8rem"
-                            }}
+                            style={{ padding: 0, fontSize: "0.8rem" }}
                           >
-                            {"ARTIST"}
+                            <i>{"List price / Daily license price"}</i>
                           </Typography>
 
-                          <Avatar className={classes.avatar}>
-                            <a
-                              href={`https://rinkeby.etherscan.io/address/${nft.artist}`}
-                              target="blank"
-                            >
-                              <Identicon string={nft.artist} size={32} />
-                            </a>
-                          </Avatar>
-                        </Box>
-                        <Box className={classes.ownerArtistBox}>
-                          <Typography
-                            gutterBottom
-                            variant="body1"
-                            className={classes.cardText}
-                            color="textSecondary"
-                            style={{
-                              padding: 0,
-                              margin: 0,
-                              fontSize: "0.8rem"
-                            }}
-                          >
-                            {"OWNER"}
-                          </Typography>
+                          <Divider style={{ backgroundColor: "black" }} />
 
-                          <Avatar className={classes.avatar}>
-                            <a
-                              href={`https://rinkeby.etherscan.io/address/${nft.owner}`}
-                              target="blank"
-                            >
-                              <Identicon string={nft.owner} size={32} />
-                            </a>
-                          </Avatar>
-                        </Box>
-                      </Box>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Box>
-            ))}
-        </Grid>
+                          <Box className={classes.ownershipBox}>
+                            <Box className={classes.ownerArtistBox}>
+                              <Typography
+                                gutterBottom
+                                variant="body1"
+                                className={classes.cardText}
+                                color="textSecondary"
+                                style={{
+                                  padding: 0,
+                                  margin: 0,
+                                  fontSize: "0.8rem"
+                                }}
+                              >
+                                {"ARTIST"}
+                              </Typography>
+
+                              <Avatar className={classes.avatar}>
+                                <a
+                                  href={`https://rinkeby.etherscan.io/address/${nft.artist}`}
+                                  target="blank"
+                                >
+                                  <Identicon string={nft.artist} size={32} />
+                                </a>
+                              </Avatar>
+                            </Box>
+                            <Box className={classes.ownerArtistBox}>
+                              <Typography
+                                gutterBottom
+                                variant="body1"
+                                className={classes.cardText}
+                                color="textSecondary"
+                                style={{
+                                  padding: 0,
+                                  margin: 0,
+                                  fontSize: "0.8rem"
+                                }}
+                              >
+                                {"OWNER"}
+                              </Typography>
+
+                              <Avatar className={classes.avatar}>
+                                <a
+                                  href={`https://rinkeby.etherscan.io/address/${nft.owner}`}
+                                  target="blank"
+                                >
+                                  <Identicon string={nft.owner} size={32} />
+                                </a>
+                              </Avatar>
+                            </Box>
+                          </Box>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </Box>
+                ))}
+            </Grid>
+          ) : (
+            <Box className={classes.emptyListBox}>
+              <Typography className={classes.emptyListText} variant="h4">
+                No NFTs!
+              </Typography>
+              <Typography className={classes.emptyListText} variant="subtitle1">
+                There are no NFTs on sale or licensable yet!
+              </Typography>
+            </Box>
+          )}
+        </>
       ) : (
         <Box className={classes.emptyListBox}>
           <Typography className={classes.emptyListText} variant="h4">
